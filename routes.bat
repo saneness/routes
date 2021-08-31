71015201de721773c1f5cfb4d8574c7ec0a326d3
saneness <45124509+saneness@users.noreply.github.com>
2021-08-31 17:25:33 +0300

    Create routes.bat

---
+++ routes.bat
@@ -0,0 +1,7 @@
+route ADD 195.82.146.214  MASK 255.255.255.255 10.8.0.1 :: rutracker.org
+route ADD 18.196.141.115  MASK 255.255.255.255 10.8.0.1 :: protonmail.com
+route ADD 3.66.223.85     MASK 255.255.255.255 10.8.0.1 :: account.protonmail.com
+route ADD 52.57.233.15    MASK 255.255.255.255 10.8.0.1 :: mail.protonmail.com
+route ADD 13.107.42.14    MASK 255.255.255.255 10.8.0.1 :: linkedin.com
+route ADD 108.174.11.37   MASK 255.255.255.255 10.8.0.1 :: ru.linkedin.com
+route ADD 172.67.148.110  MASK 255.255.255.255 10.8.0.1 :: readovka.ru
