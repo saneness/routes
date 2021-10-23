a1896d51e9e92a1ccca5635c458f767bda4f9a2f
saneness <45124509+saneness@users.noreply.github.com>
2021-10-23 19:01:21 +0300

    Update vpn.bat

--- vpn.bat
+++ vpn.bat
@@ -7,5 +7,4 @@ route ADD 172.67.70.72    MASK 255.255.255.255 10.8.0.1 :: vpsie.com
 route ADD 13.107.42.14    MASK 255.255.255.255 10.8.0.1 :: linkedin.com
 route ADD 108.174.11.37   MASK 255.255.255.255 10.8.0.1 :: ru.linkedin.com
 route ADD 195.82.146.214  MASK 255.255.255.255 10.8.0.1 :: rutracker.org
-route ADD 172.67.148.110  MASK 255.255.255.255 10.8.0.1 :: readovka.ru
 route ADD 18.194.100.90   MASK 255.255.255.255 10.8.0.1 :: gulagu.net
