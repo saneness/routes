b71b7c983e66e5a331216cd5127510d87f9f8f79
saneness <saneness@protonmail.com>
2021-12-10 22:48:56 +0300

    update script

--- routes.py
+++ routes.py
@@ -1,8 +1,50 @@
-from yaml import load
+import argparse
+import yaml
+import subprocess
+import re
 from yaml.loader import Loader
-from subprocess import check_output
-from re import compile
 
-config = load(open("routes.yml").read(), Loader=Loader)
+def args():
+    parser   = argparse.ArgumentParser(description="Generate and/or update routes.")
+    parser.add_argument("-c", "--conf", metavar="\b", default="routes.yml", help="Path to configuration file with a gateway and domains (default: routes.yml)")
+    parser.add_argument("-o", "--os", metavar="\b", default="keenetic", help="One of the operating systems: keenetic, windows (default: keenetic)")
+    parser.add_argument("-u", "--update", action="store_true", help="Use this flag to update routes on a router")
+    parser.add_argument("-r", "--router", metavar="\b", default="router", help="Hostname or ip address to update routes on (default: router)")
+    parser.add_argument("-p", "--password", metavar="\b", help="Password in case it required on the host")
+    return parser.parse_args()
 
-open("routes.bat", "w+").write("\n".join(["\n".join([f"route ADD {ip:15} MASK 255.255.255.255 {config['gateway']} :: {domain}" for ip in list(filter(compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$").match, [str(item)[2:-1] for item in check_output(["dig", "+short", domain]).split()]))]) for domain in config["domains"]]))
+def create_routes(os, domains, gateway):
+    ip_regex = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+    routes = []
+    for domain in domains:
+        ips = list(filter(ip_regex.match, [str(item)[2:-1] for item in subprocess.check_output(["dig", "+short", domain]).split()]))
+        for ip in ips:
+            route = eval(f'f"{os["template"]}"')
+            routes.append(route)
+    open("routes" + os["extension"], "w+").write("\n".join(routes))
+
+def update_routes(os, router, password):
+    routes = open("routes" + os["extension"]).read().split('\n')
+    for route in routes:
+        cmd = f"sshpass -p {password} ssh {router} {route}".split()
+        print(subprocess.check_output(cmd))
+
+if __name__ == '__main__':
+    os = {
+        "keenetic": {
+            "extension": ".keenetic",
+            "template": "ip route {ip:15} {gateway} OpenVPN0"
+        },
+        "windows": {
+            "extension": ".bat",
+            "template": "route ADD {ip:15} MASK 255.255.255.255 {gateway} :: {domain}"
+        }
+    }
+    args = args()
+    args.os = os[args.os]
+    config   = yaml.load(open(args.conf).read(), Loader=Loader)
+    domains  = config["domains"]
+    gateway  = config["gateway"]
+    create_routes(os=args.os, domains=domains, gateway=gateway)
+    if args.update:
+        update_routes(os=args.os, router=args.router, password=args.password)
